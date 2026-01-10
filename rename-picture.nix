b8ad9c47e6a3dd2da8158eb537a00b25c4314592
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2026-01-09 20:32:50 -0800

    Expand rename_cr3 to HEIC and JPG
    Signed-off-by: Jose Ulises Nino Rivera <junr03@users.noreply.github.com>

--- rename-picture.nix
+++ rename-picture.nix
@@ -1,30 +1,40 @@
-{ lib, stdenvNoCC, python3, python3Packages, exiftool, makeWrapper }:
+{
+  lib,
+  stdenvNoCC,
+  python3,
+  python3Packages,
+  exiftool,
+  makeWrapper,
+}:
 
 stdenvNoCC.mkDerivation {
-  pname = "rename-cr3";
+  pname = "rename-picture";
   version = "1.0.0";
 
   # Use the contents of this repository as the source
   src = ./.;
 
-  buildInputs = [ python3 python3Packages.tqdm makeWrapper ];
+  buildInputs = [
+    python3
+    python3Packages.tqdm
+    makeWrapper
+  ];
 
   installPhase = ''
     mkdir -p $out/bin
-    cp scripts/rename_cr3.py $out/bin/rename_cr3
-    chmod +x $out/bin/rename_cr3
+    cp scripts/rename_picture.py $out/bin/rename_picture
+    chmod +x $out/bin/rename_picture
 
     # Wrap the script to include exiftool in PATH and Python packages
-    wrapProgram $out/bin/rename_cr3 \
+    wrapProgram $out/bin/rename_picture \
       --prefix PATH : ${lib.makeBinPath [ exiftool ]} \
       --prefix PYTHONPATH : ${python3Packages.tqdm}/${python3.sitePackages}
   '';
 
   meta = with lib; {
-    description = "Script to rename .cr3/.CR3 files using datetime from EXIF data";
+    description = "Script to rename picture files using datetime from EXIF data";
     homepage = "https://github.com/junr03/gallatin";
     license = licenses.mit;
     platforms = platforms.unix;
   };
 }
-
