2edb2a0e58789935fab557c6119070b2b35d93c7
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2026-09-14 11:23:49 -0700

    Format runner RPATH definition

--- runners/github-actions-runner.nix
+++ runners/github-actions-runner.nix
@@ -66,7 +66,16 @@ stdenvNoCC.mkDerivation {
   '';
 
   postFixup = lib.optionalString stdenv.hostPlatform.isLinux ''
-    runner_rpath='$ORIGIN:${lib.makeLibraryPath [ stdenv.cc.cc curl icu krb5 openssl zlib ]}'
+    runner_rpath='$ORIGIN:${
+      lib.makeLibraryPath [
+        stdenv.cc.cc
+        curl
+        icu
+        krb5
+        openssl
+        zlib
+      ]
+    }'
     for file in "$out"/bin/*.so "$out"/bin/Runner.Listener "$out"/bin/Runner.PluginHost "$out"/bin/Runner.Worker; do
       if [ -f "$file" ]; then
         ${patchelf}/bin/patchelf --add-rpath "$runner_rpath" "$file"
